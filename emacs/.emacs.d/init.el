1136544f27780b5a0fd7238d01495eafe25a1ec6
wu <wullewutz@users.noreply.github.com>
2016-11-19 17:16:29 +0100

    Setup Rust programming environment for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
 (package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;          Look and Feel           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'color-theme)
 
 (if (display-graphic-p)
@@ -11,17 +15,36 @@
   
 (nyan-mode 1)
 (tool-bar-mode -1)
-
 (global-linum-mode 1)
-
 (set-face-attribute 'default nil :height 130)
-
 (setq inhibit-startup-message t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;          Rust dev                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; based on
+;; http://julienblanchard.com/2016/fancy-rust-development-with-emacs/
+;; https://github.com/racer-rust/emacs-racer
+(setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
+(setq racer-rust-src-path "~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src") ;; Rust source code PATH
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;         Python dev               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;       custom set vars            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,7 +52,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cargo rust-mode nyan-mode jedi helm color-theme-solarized))))
+    (company racer flycheck-rust ## cargo rust-mode nyan-mode jedi helm color-theme-solarized))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
