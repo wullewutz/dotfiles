f8239030e17b61ee06a0c953c4a12d8cc744a115
wullewutz <wullewutz@users.noreply.github.com>
2024-12-04 01:02:48 +0100

    instarch: lutris, non-interactive installation of tools

--- instarch/confarch.sh
+++ instarch/confarch.sh
@@ -20,7 +20,9 @@ sudo pacman --noconfirm -Syu
 sudo pacman ${PACMAN_ARGS} -S eza fd ripgrep wget udisks2 udiskie base-devel man-db man handlr-regex
 
 # rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# -s -- -y at the end to make it non-interactive
+# (following https://stackoverflow.com/questions/57251508/run-rustups-curl-fetched-installer-script-non-interactively)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # GNU stow (dotfiles manager)
 sudo pacman ${PACMAN_ARGS} -S stow
@@ -33,12 +35,13 @@ stow git
 # zshell
 stow zsh
 sudo pacman ${PACMAN_ARGS} -S zsh
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh $USER
 
 # neovim
 sudo pacman ${PACMAN_ARGS} -S neovim
 stow vim
 stow nvim
+nvim --headless +PlugInstall +qall
 echo "export EDITOR=/usr/bin/nvim" >> $HOME/.profile
 echo "export MANPAGER='nvim +Man!'" >> $HOME/.profile
 handlr set 'text/*' nvim.desktop
@@ -79,3 +82,15 @@ echo "export BROWSER=/usr/bin/firefox" >> $HOME/.profile
 # zathura pdf viewer
 sudo pacman ${PACMAN_ARGS} -S zathura zathura-pdf-mupdf zathura-cb tesseract-data-deu
 handlr set 'application/pdf' zathura.desktop
+
+# lutris config for starcraft gaming like it's 1998
+sudo pacman ${PACMAN_ARGS} -S lutris wine-staging
+sudo pacman ${PACMAN_ARGS} -S lib32-mesa vulkan-intel \
+                              lib32-vulkan-intel vulkan-icd-loader \
+                              lib32-vulkan-icd-loader
+sudo pacman ${PACMAN_ARGS} -S --asdeps giflib lib32-giflib gnutls lib32-gnutls \
+    v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins \
+    lib32-alsa-plugins alsa-lib lib32-alsa-lib sqlite lib32-sqlite \
+    libxcomposite lib32-libxcomposite ocl-icd lib32-ocl-icd libva lib32-libva \
+    gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs \
+    vulkan-icd-loader lib32-vulkan-icd-loader sdl2 lib32-sdl2
